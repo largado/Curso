f7d7106dede30844ecffb17845fbe7704dbb3676
largado <alexandrohipolito@gmail.com>
2022-04-30 16:47:06 -0300

    Add project files.

---
+++ Curso/Hospital.cs
@@ -0,0 +1,30 @@
+﻿
+    class Hospital
+    {
+    public string medico;
+    public string paciente;
+    public string predio;
+    public float preco;
+    public double pagamento;
+    public int notafiscal;
+
+
+    public bool Opera(double valor)
+    {
+        if (this.medico is null)
+        {
+            medico = "Sem Médico";
+            return false;
+        }
+        else
+        {
+            preco = 132;
+            return true;
+        }
+            
+
+    }
+        
+
+    }
+
---
+++ Curso/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hospital vasconselos = new Hospital();
+
+            vasconselos.paciente = "Doutor";
+
+            vasconselos.Opera(10);
+
+            Console.WriteLine(vasconselos.medico);
+            Console.WriteLine(vasconselos.preco);
+            Console.ReadKey();
+
+        }
+    }
+}
